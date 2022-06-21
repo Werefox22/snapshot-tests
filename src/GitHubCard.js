ef307b0064051f35cf12f356957ee83b335ee35c
Werefox <jack@wrightus.com>
2022-06-21 14:14:34 -0400

    Stubbed a new GitHubCard component

---
+++ src/GitHubCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from 'react-bootstrap/Card'
+
+function GitHubCard() {
+	return (
+		<Card>
+			<Card.Title>Jack Wright</Card.Title>
+		</Card>
+	)
+}
+
+export default GitHubCard
