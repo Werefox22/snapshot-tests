da17b01db21ede9d00595ebac5feb456e8a69fa2
Werefox <jack@wrightus.com>
2022-06-21 14:42:23 -0400

    Wrote a snapshot test for the github card

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import renderer from 'react-test-renderer'
+import GitHubCard from './GitHubCard';
 
 test('renders a snapshot', () => {
 	const tree = renderer.create(<App />).toJSON()
+	expect(tree).toMatchSnapshot()
+})
+
+test('renders a snapshot of the github card', () => {
+	const tree = renderer.create(<GitHubCard />).toJSON()
 	expect(tree).toMatchSnapshot()
 })
