3eeb1fb2205103b6a136f2530ac3a3f0007a31be
Werefox <jack@wrightus.com>
2022-06-21 14:40:56 -0400

    Styled github card

--- src/GitHubCard.js
+++ src/GitHubCard.js
@@ -1,10 +1,20 @@
 import React from "react";
 import Card from 'react-bootstrap/Card'
+import Image from 'react-bootstrap/Image'
 
 function GitHubCard() {
 	return (
-		<Card>
-			<Card.Title>Jack Wright</Card.Title>
+		<Card style={{
+			width: '300px',
+			margin: 'auto'
+		}}>
+			<Card.Body style={{textAlign: 'left'}}>
+				<Image src="pfp.jpg" roundedCircle thumbnail width='50px' alt='Jack Wright Profile Picture'/>
+				<Card.Title style={{display: "inline"}}> Jack Wright</Card.Title>
+				<Card.Text>
+					Software development student based in NC.
+				</Card.Text>
+			</Card.Body>
 		</Card>
 	)
 }
